571ca3530de99fd2ee2ac77a4d67c136c530e7b0
s.prokopchuk <s.prokopchuk@ddemo.ru>
2025-03-07 10:05:51 +0300

    Установил ShouldBroadcastNow

--- app/Events/MessageCreated.php
+++ app/Events/MessageCreated.php
@@ -10,9 +10,9 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class MessageCreated implements ShouldBroadcast
+class MessageCreated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
